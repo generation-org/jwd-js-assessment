b44cf66a1722a967d461a686eb298642b4e065d4
LiviaFResende <livianejaim@gmail.com>
2022-07-07 13:21:25 +1000

    count score

--- js/index.js
+++ js/index.js
@@ -7,68 +7,71 @@
   The tasks you need to do are below.
 
     TASKS TODO:
-      1. Calculate the score as the total of the number of correct answers
+      DONE  1. Calculate the score as the total of the number of correct answers
 
-      2. Add an Event listener for the submit button, which will display the score and highlight 
+      DONE 2. Add an Event listener for the submit button, which will display the score and highlight 
          the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
 
       DONE == 3. Add 2 more questions to the app (each question must have 4 options).
 
-      4. Reload the page when the reset button is clicked (hint: search window.location)
+      DONE 4. Reload the page when the reset button is clicked (hint: search window.location)
 
       5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
-window.onload = function() {
+
+// FUNCTIONS TO RELOAD THE PAGE AND SHOW QUIZ =============
+window.onload = function () {
   var reloading = sessionStorage.getItem("reloading");
   if (reloading) {
-      sessionStorage.removeItem("reloading");
-      showQuiz();
+    sessionStorage.removeItem("reloading");
+    showQuiz();
   }
 }
-
 function reloadP() {
   sessionStorage.setItem("reloading", "true");
   document.location.reload();
 }
 
-  // quizArray QUESTIONS & ANSWERS
-  // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
-  // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
-  const quizArray = [
-    {
-      q: 'Which is the third planet from the sun?',
-      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
-      a: 1, // array index 1 - so Earth is the correct answer here
-    },
-    {
-      q: 'Which is the largest ocean on Earth?',
-      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
-      a: 3,
-    },
-    {
-      q: 'What is the capital of Australia',
-      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
-      a: 1,
-    },
-    {
-      q: 'What is the biggest forest in the world?',
-      o: ['Primorye Forest', 'Tropical rainforest in Borneo', 'Amazon', 'New Guinea Tropical Rainforest'],
-      a: 2,
-    },
-    {
-      q: 'What language does people speak in Brazil?',
-      o: ['English', 'Spanish', 'Portuguese', 'Japanese'],
-      a: 2,
-    }
-  ];
 
-  // function to Display the quiz questions and answers from the object
-  const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
-    quizArray.map((quizItem, index) => {
-      quizDisplay += `<ul class="list-group">
+// quizArray QUESTIONS & ANSWERS
+// q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
+// Basic ideas from https://code-boxx.com/simple-javascript-quiz/
+const quizArray = [
+  {
+    q: 'Which is the third planet from the sun?',
+    o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
+    a: 1, // array index 1 - so Earth is the correct answer here
+  },
+  {
+    q: 'Which is the largest ocean on Earth?',
+    o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+    a: 3,
+  },
+  {
+    q: 'What is the capital of Australia',
+    o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+    a: 1,
+  },
+  {
+    q: 'What is the biggest forest in the world?',
+    o: ['Primorye Forest', 'Tropical rainforest in Borneo', 'Amazon', 'New Guinea Tropical Rainforest'],
+    a: 2,
+  },
+  {
+    q: 'What language does people speak in Brazil?',
+    o: ['English', 'Spanish', 'Portuguese', 'Japanese'],
+    a: 2,
+  }
+];
+
+
+// function to Display the quiz questions and answers from the object
+const displayQuiz = () => {
+  const quizWrap = document.querySelector('#quizWrap');
+  let quizDisplay = '';
+  quizArray.map((quizItem, index) => {
+    quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
@@ -76,36 +79,49 @@ function reloadP() {
                     <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
                     </ul>
                     <div>&nbsp;</div>`;
-      quizWrap.innerHTML = quizDisplay;
-    });
-  };
+    quizWrap.innerHTML = quizDisplay;
+  });
+};
 
-  // Calculate the score
-  const calculateScore = () => {
-    let score = 0;
-    quizArray.map((quizItem, index) => {
-      for (let i = 0; i < 4; i++) {
-        //highlight the li if it is the correct answer
-        let li = `li_${index}_${i}`;
-        let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
 
-        if (quizItem.a == i) {
-          //change background color of li element here
-        }
 
-        if (radioElement.checked) {
-          // code for task 1 goes here
-        }
+// Calculate the score
+const calculateScore = () => {
+  let score = 0;
+  quizArray.map((quizItem, index) => {
+    for (let i = 0; i < 4; i++) {
+
+      //highlight the li if it is the correct answer
+      let li = `li_${index}_${i}`;
+      let r = `radio_${index}_${i}`;
+      liElement = document.querySelector('#' + li);
+      radioElement = document.querySelector('#' + r);
+
+      if (quizItem.a == i) {
+        //change background color of li element here
+        liElement.style.backgroundColor = "lightyellow";
       }
-    });
-  };
+
+      if (radioElement.checked) {
+        if (i == quizItem.a) {
+          score++;
+          console.log(`Score is : ${score}`);
+        }
+        // code for task 1 goes here
+      }
+    }
+  });
+let scoreDiv = document.querySelector('#score');
+scoreDiv.innerHTML = `Your total score is ${score}`;
+
+};
 
 
-  function showQuiz() {
-      document.querySelector('#quizBlock').style.display = 'block';
-      start.style.display = 'none';
-      displayQuiz();
-  }
+
+// SHOW THE QUIZ IN THE PAGE 
+function showQuiz() {
+  document.querySelector('#quizBlock').style.display = 'block';
+  start.style.display = 'none';
+  displayQuiz();
+}
 
